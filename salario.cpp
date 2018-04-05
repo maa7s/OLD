c8b1216b582596ed772ea916d46a4f9c1d55d4b7
maa7s <38090686+maa7s@users.noreply.github.com>
2018-04-04 23:05:15 -0300

    Create salario.cpp

---
+++ salario.cpp
@@ -0,0 +1,17 @@
+# luuuul#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+  double ma,id,sb,sbr,sbg,sl;//sbr=salario reajustado e sbg=salario gratificado
+  cout<<"Digite sseu numero de matricula,idade,e salario bruto ,respectivamente"<<endl;cin>>ma>>id>>sb;
+  double reaj=0.38,grat=1.20,desc=0.85;
+ 
+  sbr=(sb*reaj)+sb;  
+  sbg=sbr*grat;
+  sl=sbg*desc;
+  
+  cout<<"Numero de Matricula "<<ma<<endl;cout<<"Salario Bruto "<<sbg<<endl;cout<<"Salario Liquido"<<sl;
+
+return 0;
